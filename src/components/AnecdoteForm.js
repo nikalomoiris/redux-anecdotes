90f511a3b00e22a74dd80affe26b57491ea2d541
nikalomiris <nikalomiris@gmail.com>
2019-08-05 14:47:09 -0400

    Part 6 Better anecdotes step 11 solution

--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { showNotification, hideNotification } from '../reducers/notificationReducer'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteForm = (props) => {
     const addAnecdote = (event) => {
-      event.preventDefault()
-      const content = event.target.anecdote.value
-    //   store.dispatch(createAnecdote(content))
+        event.preventDefault()
+        const content = event.target.anecdote.value
+        event.target.anecdote.value = ''
+        props.createAnecdote(content)
     //   store.dispatch(showNotification(`you created '${content}'`))
       setTimeout(() => {
         // store.dispatch(hideNotification())
@@ -24,4 +26,8 @@ const AnecdoteForm = (props) => {
     )
 }
 
-export default AnecdoteForm
+const mapDispatchToProps = {
+    createAnecdote
+}
+
+export default connect(null, mapDispatchToProps)(AnecdoteForm)
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { newFilter } from '../reducers/filterReducer'
 
 const Filter = (props) => {
     return (
       <>
-        {/* <div>filter <input value={store.getState().filter}
-          onChange={(event) => store.dispatch(newFilter(event.target.value))} /></div> */}
+        <div>filter <input value={props.filter}
+          onChange={(event) => props.newFilter(event.target.value)} /></div>
       </>
     )
 }
 
-export default Filter
+const mapStateToProps = (state) => {
+    return {
+        filter: state.filter
+    }
+}
+
+const mapDispatchToProps = {
+    newFilter
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Filter)
